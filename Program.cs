be96a972682caa180d7d417f3b6fad0fa927e716
lucas augusto <luaugustomonteiror@gmail.com>
2026-09-04 19:54:05 -0300

    continue finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Pressione uma tecla para continuar...");
+
+Console.ReadKey();
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Muito Obrigado, Finalizar Programa...");
+Console.ResetColor();
+
+Thread.Sleep(1000);
+Console.Clear();
